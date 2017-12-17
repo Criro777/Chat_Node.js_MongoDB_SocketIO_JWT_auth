8613c694a1ac5bc16e40c0d97b60e48a1f3d69a2
Unknown <russiancardcollector@mail.ru>
2017-12-18 03:24:38 +0400

    Подключены passport и passport-jwt

--- server/server.js
+++ server/server.js
@@ -12,6 +12,15 @@ nunjucks.configure('./client/views', {
     express: app
 });
 
+const passport = require('passport');
+const {Strategy} = require('passport-jwt');
+const {jwt} = require('./config');
+
+passport.use(new Strategy(jwt, function (jwt_payload, done) {
+    if (jwt_payload != void(0)) return done(false, jwt_payload);
+    done();
+}));
+
 mongoose.connect('mongodb://localhost:27017/chat', {useMongoClient: true});
 mongoose.Promise = require('bluebird');
 mongoose.set('debug', true);
