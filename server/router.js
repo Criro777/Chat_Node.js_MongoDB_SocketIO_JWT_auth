b87fb8e8d48b410730e82f267c2724836a388a93
Unknown <russiancardcollector@mail.ru>
2017-12-18 03:25:38 +0400

    Добавлен посредник для ограничения входа незарегестрированных пользователей

--- server/router.js
+++ server/router.js
@@ -1,11 +1,20 @@
 "use strict";
 
 const express = require('express');
+const passport = require('passport');
+
+function checkAuth(req, res, next) {
+    passport.authenticate('jwt', {session: false}, (err, decryptToken, jwtError) => {
+        if (jwtError != void(0) || err != void(0)) return res.render('index.html', {error: err || jwtError});
+        req.user = decryptToken;
+        next();
+    })(req, res, next);
+}
 
 module.exports = app => {
     app.use('/assets', express.static('./client/public'));
 
-    app.get('/', (req, res) => {
-        res.render('index.html');
+    app.get('/', checkAuth, (req, res) => {
+        res.render('index.html', {username: req.user.username});
     });
 }
