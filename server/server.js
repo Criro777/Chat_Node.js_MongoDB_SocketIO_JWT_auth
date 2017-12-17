b4cf16aeb67b42c862d6ee8f62afe06fa1517bf7
Unknown <russiancardcollector@mail.ru>
2017-12-18 01:48:23 +0400

    Подключена MongoDB

--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server, {serverClient: true});
+const mongoose = require('mongoose');
 const nunjucks = require('nunjucks');
 
 nunjucks.configure('./client/views', {
     autoescape: true,
     express: app
 });
+
+mongoose.connect('mongodb://localhost:27017/chat', {useMongoClient: true});
+mongoose.Promise = require('bluebird');
+mongoose.set('debug', true);
 
 require('./sockets')(io);
 require('./router')(app);
